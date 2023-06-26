be66452dddbaa2c3dd99a850c562e293eac4f3c4
xiadengmin <xiadengmin@gmail.com>
2023-06-26 11:05:51 +0800

    Second

--- mainwidget.cpp
+++ mainwidget.cpp
@@ -6,7 +6,11 @@ MainWidget::MainWidget(QWidget *parent) :
     ui(new Ui::MainWidget)
 {
     ui->setupUi(this);
-    ui->tableWidget->setRowCount(10);
+    ui->tableWidget->setRowCount(10);//表格设置10列
+//    ui->tableWidget->setColumnWidth(0,50); //设置自定义列宽
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch); //x先自适应宽度
+    ui->tableWidget->horizontalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);//根据内容使用宽度的列
+    ui->tableWidget->horizontalHeader()->setStyleSheet("QHeaderView::section{background:skyblue;color: black;}");//修复表头网格线丢失
     //重置窗口大小
     resize(1300,800);
     //固定窗口大小
