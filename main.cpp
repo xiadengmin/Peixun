eefb6e7ac18af3e7b5bc073d147f7ba8d2e5895f
xiadengmin <xiadengmin@gmail.com>
2023-06-27 02:20:41 +0800

    Third_commit

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include "mainwidget.h"
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include<QDebug>
 #include <QApplication>
+#include <QSqlError>
+
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+
+
+    //连接MySQL数据库
+    qDebug()<<QSqlDatabase::drivers();
+    QString dsn = "Driver={MySQL ODBC 8.0 UniCode Driver};"; // Driver is now {MySQL}
+        dsn.append("Server=127.0.0.1,3306;"); // IP,Port
+        dsn.append("Database=mysql;");  // Schema
+        dsn.append("Uid=root;");           // User
+        dsn.append("Pwd=root;");
+        dsn.append("MULTI_HOST=1;");
+
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setDatabaseName(dsn);
+
+    if (!db.open())
+       qDebug()<<"can't open!"<<db.lastError();
+    else
+         qDebug()<<"open!";
+    QSqlQuery result = db.exec("select user from user;");
+    while (result.next())
+        qDebug() << result.value("user").toString();
+
+
+
     MainWidget w;
     w.show();
 
--- mainwidget.h
+++ mainwidget.h
@@ -2,6 +2,7 @@
 #define MAINWIDGET_H
 
 #include <QWidget>
+#include<QSqlDatabase>
 
 namespace Ui {
 class MainWidget;
@@ -14,6 +15,11 @@ class MainWidget : public QWidget
 public:
     explicit MainWidget(QWidget *parent = 0);
     ~MainWidget();
+
+private slots:
+    void on_comboBox_currentIndexChanged(int index);
+
+    void on_tabWidget_currentChanged(int index);
 
 private:
     Ui::MainWidget *ui;
